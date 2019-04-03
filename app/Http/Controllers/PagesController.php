ae08d4c96bf51d42b44d055fdf8338b34852bcd9
AboodMadridista <41301312+AboodMadridista@users.noreply.github.com>
2019-04-03 16:43:44 +0800

    Initial commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function homepage()
+    {
+        return view('pages.homepage');
+    }
+    public function login()
+    {
+        return view('pages.login');
+    }
+
+}
---
+++ database/migrations/2019_04_03_081815_create_tenants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTenantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tenants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->String('name',40);
+            $table->string('LotNo',10)->index();
+            $table->string('Level', 10)->index();
+            $table->string('Zone', 10)->index();
+            $table->string('Category', 15)->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tenants');
+    }
+}
